b573b746ece07775f074a473c9a6d8b41969c4f8
Diogo Rossi <rossi.diogo@gmail.com>
2026-03-26 21:31:46 -0300

    ✨ feat: populate the module

--- src/myunits/__init__.py
+++ src/myunits/__init__.py
@@ -1,2 +1,58 @@
-def hello() -> str:
-    return "Hello from myunits!"
+# units
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+
+# Mass
+kg = ureg.kg
+g = ureg.g
+
+# Time
+d = ureg.d
+s = ureg.s
+us = ureg.us
+
+# Distance
+m = ureg.m
+cm = ureg.cm
+mm = ureg.mm
+um = ureg.um
+ft = ureg.ft
+
+# Volume
+bbl = ureg.oil_bbl
+
+# Pressure
+Pa = ureg.Pa
+kPa = ureg.kPa
+MPa = ureg.MPa
+GPa = ureg.GPa
+psi = ureg.psi
+bar = ureg.bar
+
+# Temperature
+K = ureg.kelvin
+R = ureg.rankine
+oC = ureg.degC
+oF = ureg.degF
+
+# Force
+kgf = ureg.kgf
+N = ureg.N
+daN = ureg.daN
+kN = ureg.kN
+MN = ureg.MN
+GN = ureg.GN
+
+# Permeability
+D = ureg.darcy
+mD = 0.001 * D
+ureg.define("mD = 0.001 * darcy")
+
+# Viscosity
+P = ureg.poise
+cP = ureg.cP
+
+# Energy
+J = ureg.J
+W = ureg.W
